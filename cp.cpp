639ba04ccdec67b5ff8eea97dbff5ed75d8835bd
MD. Sajid Hossain <69637820+sajidrec@users.noreply.github.com>
2022-04-28 21:54:22 +0600

    time complexity added in comment and some changes

--- cp.cpp
+++ cp.cpp
@@ -6,17 +6,41 @@ typedef long long int lld;
 
 lld binpow(lld base, lld power)
 {
-    if (!power)
-        return 1;
-    lld res = binpow(base, power / 2);
-    if (power % 2)
-        return res * res * base;
-    else
-        return res * res;
+    //** Time complexity O(log(n)) **//
+
+    // if power is odd then res = res*base and power--
+    // if power if even then base = base*base and power = power/2
+    // if power is zero then res will be the answer
+
+    lld res = 1;
+
+    while (power)
+    {
+        if (power == 1)
+        {
+            res *= base;
+            power = 0;
+            break;
+        }
+        if (power % 2 == 0)
+        {
+            base *= base;
+            power /= 2;
+        }
+        else
+        {
+            res *= base;
+            power--;
+        }
+    }
+
+    return res;
 }
 
 bool isPrime(lld num)
 {
+    // Time complexity O(sqrt(n))
+
     if (num == 1 || (num % 2 == 0 && num != 2))
     {
         return false;
