eac795e69255492a76dd63dfffb8ee34e77dc774
MD. Sajid Hossain <69637820+sajidrec@users.noreply.github.com>
2024-12-28 01:07:48 +0600

    Update cp.cpp (removed some functions)

--- cp.cpp
+++ cp.cpp
@@ -5,87 +5,6 @@ using namespace std;
 typedef long long int lld;
 typedef string String;
 
-using int128 = signed __int128;
-using uint128 = unsigned __int128;
-
-// int128 variableName;//  for 128 bit int
-// uint128 variableName;// for unsigned 128 bit int
-namespace int128_io
-{
-
-    inline auto char_to_digit(int chr)
-    {
-        return static_cast<int>(isalpha(chr) ? 10 + tolower(chr) - 'a' : chr - '0');
-    }
-
-    inline auto digit_to_char(int digit)
-    {
-        return static_cast<char>(digit > 9 ? 'a' + digit - 10 : '0' + digit);
-    }
-
-    template <class integer>
-    inline auto to_int(const std::string &str, size_t *idx = nullptr, int base = 10)
-    {
-        size_t i = idx != nullptr ? *idx : 0;
-        const auto n = str.size();
-        const auto neg = str[i] == '-';
-        integer num = 0;
-        if (neg)
-            ++i;
-        while (i < n)
-            num *= base, num += char_to_digit(str[i++]);
-        if (idx != nullptr)
-            *idx = i;
-        return neg ? -num : num;
-    }
-
-    template <class integer>
-    inline auto to_string(integer num, int base = 10)
-    {
-        const auto neg = num < 0;
-        std::string str;
-        if (neg)
-            num = -num;
-        do
-            str += digit_to_char(num % base), num /= base;
-        while (num > 0);
-        if (neg)
-            str += '-';
-        std::reverse(str.begin(), str.end());
-        return str;
-    }
-
-    inline auto next_str(std::istream &stream)
-    {
-        std::string str;
-        stream >> str;
-        return str;
-    }
-
-    template <class integer>
-    inline auto &read(std::istream &stream, integer &num)
-    {
-        num = to_int<integer>(next_str(stream));
-        return stream;
-    }
-
-    template <class integer>
-    inline auto &write(std::ostream &stream, integer num) { return stream << to_string(num); }
-}
-
-inline auto &operator>>(istream &stream, int128 &num) { return int128_io::read(stream, num); }
-inline auto &operator>>(istream &stream, uint128 &num) { return int128_io::read(stream, num); }
-inline auto &operator<<(ostream &stream, int128 num) { return int128_io::write(stream, num); }
-inline auto &operator<<(ostream &stream, uint128 num) { return int128_io::write(stream, num); }
-
-inline auto uint128_max()
-{
-    uint128 ans = 0;
-    for (uint128 pow = 1; pow > 0; pow <<= 1)
-        ans |= pow;
-    return ans;
-}
-
 lld binpowModular(lld base, lld power, lld Mod)
 {
     //** Time complexity O(log(n)) **//
@@ -138,48 +57,6 @@ lld binpow(lld base, lld power)
     }
 
     return res;
-}
-
-bool isPrime(lld num)
-{
-    // Time complexity O(sqrt(n))
-
-    if (num == 1 || (num % 2 == 0 && num != 2))
-    {
-        return false;
-    }
-    for (lld i = 2; i * i <= num; i++)
-    {
-        if (num % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-template <class T>
-lld binSearch(vector<T> &vec, lld left, lld right, T target)
-{
-    // to call binSearch<data type> (params);
-    // time complexity O(log(N))
-    if (left > right)
-    {
-        return -1;
-    }
-    lld mid = (left + right) / 2;
-    if (vec[mid] == target)
-    {
-        return mid;
-    }
-    else if (vec[mid] > target)
-    {
-        return binSearch(vec, left, mid - 1, target);
-    }
-    else
-    {
-        return binSearch(vec, mid + 1, right, target);
-    }
 }
 
 // seive generate
